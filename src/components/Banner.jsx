89a355d7109a5560070a532654bab4cccd864c33
fayshal69 <foyshalbinamir@gmail.com>
2024-05-09 22:34:55 +0600

    added banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,10 @@
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
 const Banner = () => {
     return (
         // <div className="py-6 md:py-8 lg:py-12 flex flex-col lg:flex-row gap-6 md:gap-8 lg:gap-12">
@@ -5,7 +12,7 @@ const Banner = () => {
 
         //     </div>
         // </div>
-        <div className="hero h-[650px] rounded-lg overflow-hidden bg-heroBg">
+        <div className="hero h-[750px] rounded-lg overflow-hidden bg-heroBg">
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="flex flex-col lg:flex-row gap-6 md:gap-8 lg:gap-12 w-full">
                 <div className="hero-content text-center lg:text-left text-white lg:w-1/2">
@@ -15,8 +22,37 @@ const Banner = () => {
                         <button className="btn bg-[#41B06E] border-none text-white text-lg">Explore</button>
                     </div>
                 </div>
-                <div className="">
-
+                <div className="px-5 md:w-[75%] mx-auto lg:w-1/2 lg:mr-12">
+                    <Swiper
+                        slidesPerView={'auto'}
+                        spaceBetween={30}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        loop={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Pagination, Autoplay]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <img src="/slide1.jpg" alt="slide img" className='w-full h-[400px] object-cover rounded-lg' />
+                        </SwiperSlide>
+                        <SwiperSlide className='h-1/2'>
+                            <img src="/slide2.jpg" alt="slide img" className='w-full h-[400px] object-cover rounded-lg' />
+                        </SwiperSlide>
+                        <SwiperSlide className='h-1/2'>
+                            <img src="/slide3.jpg" alt="slide img" className='w-full h-[400px] object-cover rounded-lg' />
+                        </SwiperSlide>
+                        <SwiperSlide className='h-1/2'>
+                            <img src="/slide4.jpg" alt="slide img" className='w-full h-[400px] object-cover rounded-lg' />
+                        </SwiperSlide>
+                        <SwiperSlide className='h-1/2'>
+                            <img src="/slide5.jpg" alt="slide img" className='w-full h-[400px] object-cover rounded-lg' />
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
             </div>
         </div>
