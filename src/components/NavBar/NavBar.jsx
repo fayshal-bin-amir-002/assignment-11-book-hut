1a48ebb0227bff0bfb4a8e6156e353bc64d8d1c2
fayshal69 <foyshalbinamir@gmail.com>
2024-05-11 17:45:19 +0600

    updated navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -48,7 +48,7 @@ const NavBar = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </div>
-                    <ul tabIndex={0} className="space-y-2 menu-sm dropdown-content mt-12 z-[1] p-2 shadow bg-[#FFF5E0] rounded-box w-52">
+                    <ul tabIndex={0} className="space-y-2 menu menu-sm dropdown-content mt-12 z-[1] p-2 shadow bg-[#FFF5E0] rounded-box w-44 font-medium">
                         {navLinks}
                     </ul>
 
